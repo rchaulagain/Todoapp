57fe1a0086a037cfbcaeae4b854dd22a42f040e8
sprasains1 <sabal_prasain@hotmail.com>
2014-04-02 15:26:47 -0700

    newcommit

--- todoapp-day3/JavaScript1.js
+++ todoapp-day3/JavaScript1.js
@@ -1,20 +1,22 @@
 ﻿
 
-function myfunct(){
+function buttonClick(){
     "use strict";
-
-var fakeItemsArray =["Real fake", " Fake fake", "artifially fake", "born fake"]
-
-
-
-
-
-var message = document.getElementById('rf');
-message.innerHTML = strikethough;
     
-    
+
+
+    var fakeItemsArray = ["Real fake", " Fake fake", "artifially fake", "born fake"]
+    document.getElementById("sel").innerHTML = " Please the fake item you want to purchase"; 
+
+
+    var selecteditem1 = document.getElementById("todoInput").value;
+    document.getElementById("listeditems").innerHTML += "<li>"+ selecteditem1+"</li>"
+
+
+
+     
   
-document.getElementById("todoInput");
+    document.getElementById("todoInput");
 
        
 }
