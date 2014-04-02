849bcc238b2dc3a7766e156aee57807bfb8e12a3
sprasains1 <sabal_prasain@hotmail.com>
2014-04-02 16:57:27 -0700

    dass wassup !!!

--- todoapp-day3/JavaScript1.js
+++ todoapp-day3/JavaScript1.js
@@ -1,5 +1,5 @@
 ﻿
-
+var counter = 0;
 function buttonClick(){
     "use strict";
     
@@ -7,16 +7,17 @@ function buttonClick(){
 
     var fakeItemsArray = ["Real fake", " Fake fake", "artifially fake", "born fake"]
     document.getElementById("sel").innerHTML = " Please the fake item you want to purchase"; 
-
+    
 
     var selecteditem1 = document.getElementById("todoInput").value;
-    document.getElementById("listeditems").innerHTML += "<li>"+ selecteditem1+"</li>"
-
-
-
-     
-  
-    document.getElementById("todoInput");
-
+    document.getElementById("listeditems").innerHTML += '<li id="selecteditem' + counter + '" onclick="strikeselected(\'selecteditem'+counter+'\');">' + selecteditem1 + '</li>';
+    counter++;
+  document.getElementById("todoInput");
        
+}
+
+function strikeselected(listID) {
+
+    var ele = document.getElementById(listID);
+    ele.style.setProperty("text-decoration", "line-through");
 }
