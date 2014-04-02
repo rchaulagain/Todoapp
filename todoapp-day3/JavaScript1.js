3e48fe2bb62e07a2323a878779b927ebfee0bed3
sprasains1 <sabal_prasain@hotmail.com>
2014-04-02 14:23:41 -0700

    fake items added

--- todoapp-day3/JavaScript1.js
+++ todoapp-day3/JavaScript1.js
@@ -1,1 +1,20 @@
 ﻿
+
+function myfunct(){
+    "use strict";
+
+var fakeItemsArray =["Real fake", " Fake fake", "artifially fake", "born fake"]
+
+
+
+
+
+var message = document.getElementById('rf');
+message.innerHTML = strikethough;
+    
+    
+  
+document.getElementById("todoInput");
+
+       
+}
